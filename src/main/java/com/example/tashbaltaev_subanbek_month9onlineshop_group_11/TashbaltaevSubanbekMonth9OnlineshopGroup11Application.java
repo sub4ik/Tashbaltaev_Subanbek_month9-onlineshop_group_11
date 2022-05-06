348980aa104eb201239c0ab2d8941a3b90c2fe47
Subanbek <stashbaltaev@gmail.com>
2022-05-06 18:18:45 +0600

    init

---
+++ src/main/java/com/example/tashbaltaev_subanbek_month9onlineshop_group_11/TashbaltaevSubanbekMonth9OnlineshopGroup11Application.java
@@ -0,0 +1,13 @@
+package com.example.tashbaltaev_subanbek_month9onlineshop_group_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TashbaltaevSubanbekMonth9OnlineshopGroup11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TashbaltaevSubanbekMonth9OnlineshopGroup11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tashbaltaev_subanbek_month9onlineshop_group_11/TashbaltaevSubanbekMonth9OnlineshopGroup11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tashbaltaev_subanbek_month9onlineshop_group_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TashbaltaevSubanbekMonth9OnlineshopGroup11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
